0f97b2112ab8ce110ce316fda215b231308a5de2
Evgenii Novikov <disregard.the.rest@gmail.com>
2025-08-10 12:59:32 +0300

    serverpod version update

--- dartway_core_serverpod_flutter/lib/core/dw_core_setup_extension.dart
+++ dartway_core_serverpod_flutter/lib/core/dw_core_setup_extension.dart
@@ -2,7 +2,6 @@ import 'package:collection/collection.dart';
 import 'package:dartway_core_serverpod_client/dartway_core_serverpod_client.dart'
     as dartway;
 import 'package:dartway_core_serverpod_flutter/dartway_core_serverpod_flutter.dart';
-import 'package:dartway_core_serverpod_flutter/session_state/dw_session_state.dart';
 import 'package:dartway_core_serverpod_flutter/socket_state/dw_socket_state.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:serverpod_auth_client/serverpod_auth_client.dart' as auth;
@@ -14,7 +13,7 @@ extension DwCoreSetupExtension on WidgetRef {
   initDartwayServerpodApp<UserProfileClass extends SerializableModel>({
     required ServerpodClientShared client,
     required Function() initRepositoryFunction,
-    UserIdMode userIdMode = UserIdMode.userInfoId,
+    UserIdMode userIdMode = UserIdMode.userProfileId,
     // List<NitRepositoryDescriptor>? customRepositoryDescriptors,
     // NitAuthConfig? nitAuthConfig,
   }) async {
@@ -22,9 +21,13 @@ extension DwCoreSetupExtension on WidgetRef {
       (e) => e is dartway.Caller,
     );
 
-    if (dartwayCaller != null) {
-      DwCore.endpointCaller = dartwayCaller as dartway.Caller;
+    if (dartwayCaller == null) {
+      throw Exception(
+        'Dartway Core module not enabled, can not init app. Add dartway_core_serverpod module to the client',
+      );
     }
+
+    DwCore.endpointCaller = dartwayCaller as dartway.Caller;
 
     final authCaller = client.moduleLookup.values.firstWhereOrNull(
       (e) => e is auth.Caller,
@@ -32,68 +35,21 @@ extension DwCoreSetupExtension on WidgetRef {
 
     if (authCaller == null) {
       throw Exception(
-        'Auth module not enabled, can not init session. Add serverpod_auth module to the client',
+        'Auth module not enabled, can not init session. Add dartway_auth_serverpod or serverpod_auth module to the client',
       );
     }
-
-    // if (authCaller != null) {
-    //   authModuleCaller = authCaller as auth.Caller;
-    // }
-
-    // authModuleCaller = client!.moduleLookup.values
-    //     .firstWhereOrNull((e) => e is auth.Caller) as auth.Caller;
-
-    // final chatsCaller = client!.moduleLookup.values
-    //     .firstWhereOrNull((e) => e is chats.Caller);
-
-    // if (chatsCaller != null) {
-    //   chatsModuleCaller = chatsCaller as chats.Caller;
-    // }
-    // chatsModuleCaller = client!.moduleLookup.values
-    //     .firstWhereOrNull((e) => e is chats.Caller) as chats.Caller;
 
     DwCoreServerpodClient.protocol = client.serializationManager;
 
     await initRepositoryFunction();
 
-    // for (var repo
-    //     in customRepositoryDescriptors ?? <NitRepositoryDescriptor>[]) {
-    //   repo.init();
-    // }
-
     await read(dwSessionStateProvider.notifier).init(
       authModuleCaller: authCaller as auth.Caller,
       signedInUserIdPreloadProcessing: (serverpodUserInfoId) async {
         if (serverpodUserInfoId != null) {
-          // final profile = await DwCore.endpointCaller.dwCrud
-          //     .getOne(
-          //       className: 'UserProfile',
-          //       filter: DwBackendFilter<int>.value(
-          //         type: DwBackendFilterType.equals,
-          //         fieldName: 'userId',
-          //         fieldValue: serverpodUserInfoId,
-          //       ),
-          //     )
-          //     .then((response) => processApiResponse<DwModelWrapper>(response));
-
           final userProfile = await readModelCustom<UserProfileClass>(
             backendFilter: DwCore.prepareUserProfileFilter(serverpodUserInfoId),
           );
-
-          // final t = read(
-          //   DwRepository.singleEntityProvider<UserProfileClass>()(
-          //         DwSingleEntityStateConfig(
-          //           backendFilter: DwBackendFilter<int>.value(
-          //             type: DwBackendFilterType.equals,
-          //             fieldName: 'id',
-          //             fieldValue: serverpodUserInfoId,
-          //           ),
-          //         ),
-          //       )
-          //       .notifier,
-          // );
-
-          // final p2 = await readModel<UserProfileClass>((p as dynamic).id);
 
           return userIdMode == UserIdMode.userProfileId
               ? (userProfile as dynamic).id
@@ -102,8 +58,6 @@ extension DwCoreSetupExtension on WidgetRef {
         return null;
       },
     );
-
-    // if (nitAuthConfig != null) NitAuthConfig.config = nitAuthConfig;
 
     return read(dwSocketStateProvider.notifier).init(client: client);
   }
