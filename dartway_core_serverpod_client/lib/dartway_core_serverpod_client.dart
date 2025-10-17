5dccde124e380ce5b183e081ac36515d3e116b8a
Evgenii Novikov <disregard.the.rest@gmail.com>
2025-10-17 09:22:04 +0300

    exports fix

--- dartway_core_serverpod_client/lib/dartway_core_serverpod_client.dart
+++ dartway_core_serverpod_client/lib/dartway_core_serverpod_client.dart
@@ -3,20 +3,21 @@ import 'package:serverpod_client/serverpod_client.dart'
 
 export 'package:serverpod_client/serverpod_client.dart';
 
+export 'src/protocol/protocol.dart';
 export 'src/domain/extra_classes/dw_api_response.dart';
 export 'src/domain/extra_classes/dw_backend_filter.dart';
 export 'src/domain/extra_classes/dw_model_wrapper.dart';
-export 'src/protocol/dw_backend_filter_type.dart';
-export 'src/protocol/auth/dw_auth_provider.dart';
-export 'src/protocol/auth/dw_auth_request.dart';
-export 'src/protocol/auth/dw_auth_request_status.dart';
-export 'src/protocol/auth/dw_auth_request_type.dart';
-export 'src/protocol/auth/dw_auth_verification.dart';
-export 'src/protocol/auth/dw_auth_verification_type.dart';
-export 'src/protocol/dw_app_notification.dart';
-export 'src/protocol/dw_updates_transport.dart';
-export 'src/protocol/media/dw_media.dart';
-export 'src/protocol/media/dw_media_type.dart';
+// export 'src/protocol/dw_backend_filter_type.dart';
+// export 'src/protocol/auth/dw_auth_provider.dart';
+// export 'src/protocol/auth/dw_auth_request.dart';
+// export 'src/protocol/auth/dw_auth_request_status.dart';
+// export 'src/protocol/auth/dw_auth_request_type.dart';
+// export 'src/protocol/auth/dw_auth_verification.dart';
+// export 'src/protocol/auth/dw_auth_verification_type.dart';
+// export 'src/protocol/dw_app_notification.dart';
+// export 'src/protocol/dw_updates_transport.dart';
+// export 'src/protocol/media/dw_media.dart';
+// export 'src/protocol/media/dw_media_type.dart';
 
 class DwCoreServerpodClient {
   static late final SerializationManager protocol;
